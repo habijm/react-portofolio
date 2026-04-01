b9b38c943ce89195cb77868e1e4b6453a6bf4788
habijm <habijiyan111@gmail.com>
2026-04-01 13:20:29 +0700

    add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import DataImage from "./data";
+
 function App() {
  
   return (
-   <h1 className="text-2xl font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quasi aspernatur ducimus consequuntur consectetur magni porro saepe in deserunt autem illo magnam natus, qui aut soluta maiores doloremque, dolorem expedita harum laborum? In perspiciatis vero sequi exercitationem libero sint mollitia, veniam minus quam nihil recusandae saepe suscipit nobis veritatis amet. Quas similique maxime id sequi dicta optio at amet nesciunt ab aliquid quidem voluptas saepe eos ullam ducimus dolorum nostrum, expedita, voluptates architecto blanditiis corporis provident voluptatibus! Totam possimus repellat nihil temporibus perspiciatis veritatis, autem blanditiis! Iste illum minus quae! Accusamus quidem, molestias itaque enim sed labore pariatur dolore vero soluta quas, incidunt sit ullam distinctio nobis voluptas ducimus! Sunt ab quas quod fuga commodi omnis aperiam eius! Blanditiis dicta voluptatum delectus eaque numquam inventore deleniti labore, amet itaque neque temporibus fuga quaerat distinctio debitis mollitia eveniet consectetur? Dignissimos excepturi praesentium ducimus est nostrum doloremque ex minus, at modi aliquam totam. Delectus dolorem eligendi, quisquam officiis error obcaecati dolorum! Repellat laborum animi architecto, excepturi consectetur corporis. Accusamus, id dolor veritatis obcaecati culpa recusandae nostrum cumque minus natus consectetur ipsam consequuntur earum magnam et nesciunt dicta, libero impedit animi. Corporis accusantium soluta ea repellat, architecto quia amet aspernatur accusamus laboriosam molestiae.</h1>
+   <div className="hero grid md:grid-cols-2 pt-10 items-center xl:gap-0 gap-6 grid-cols-1">
+    <div>
+      <div className="flex items-center gap-3 mb-6 bg-zinc-800 w-fit p-4 rounded-2xl">
+        <img src={DataImage.HeroImage} alt="Hero Image" className="w-10 rounded-md"/>
+        <q>Kode yang indah, lahir dari ketekunan.😁</q>
+      </div>
+      <h1 className="text-3xl/tight font-bold mb-6">Hi, Saya Habi Jiyan Mustaqim</h1>
+      <p className="text-base/loose mb-6 opacity-50">
+        Seorang pengembang perangkat lunak yang bersemangat dan bertanggung jawab. Saya senang membuat solusi yang inovatif dan efisien untuk masalah yang kompleks.
+      </p>
+      <div className="flex items-center sm:gap-4 gap-2">
+        <a href="#" className="bg-violet-700 p-4 rounded-2xl hover:bg-violet-600 transition-colors">Download CV <i className="ri-download-line ri-lg"></i> </a>
+        <a href="#" className="bg-zinc-700 p-4 rounded-2xl hover:bg-zinc-600 transition-colors">Lihat Project <i className="ri-arrow-right-line ri-lg"></i></a>
+      </div>
+    </div>
+    <img src={DataImage.HeroImage} alt="Hero Image" className="w-[500px] md:ml-auto" />
+   </div>
   )
 }
 
